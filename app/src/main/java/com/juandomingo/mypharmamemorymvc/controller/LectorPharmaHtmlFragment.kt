29349f7adfef584176e2134468616847b67133a4
juand <domingoortin.j@gmail.com>
2021-11-29 00:45:29 +0100

    Pharma national code extracted

--- app/src/main/java/com/juandomingo/mypharmamemorymvc/controller/LectorPharmaHtmlFragment.kt
+++ app/src/main/java/com/juandomingo/mypharmamemorymvc/controller/LectorPharmaHtmlFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentResultListener
 import com.juandomingo.mypharmamemorymvc.R
 import com.juandomingo.mypharmamemorymvc.databinding.FragmentLectorPharmaHtmlBinding
+import com.juandomingo.mypharmamemorymvc.model.Context
+import java.util.stream.IntStream.range
 
 
 class LectorPharmaHtmlFragment : Fragment(R.layout.fragment_lector_pharma_html) {
@@ -16,6 +19,7 @@ class LectorPharmaHtmlFragment : Fragment(R.layout.fragment_lector_pharma_html) 
     private lateinit var readCodeTv: TextView
     private val requestKey = "REQUEST_KEY"
     private val code = "CODE"
+    private lateinit var pharmaNatCode: String
     companion object {
         fun newInstance() = LectorPharmaHtmlFragment()
     }
@@ -25,11 +29,45 @@ class LectorPharmaHtmlFragment : Fragment(R.layout.fragment_lector_pharma_html) 
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentLectorPharmaHtmlBinding.bind(view)
         readCodeTv = binding.readedCodeTv
-
-        parentFragmentManager.setFragmentResultListener(requestKey, this) { requestKey: String, bundle: Bundle ->
-            val codeValue = bundle.getString(code)
-            readCodeTv.setText(codeValue).toString()
-        }
+        receiveFullPharmaCode()
+        // https://cima.aemps.es/cima/rest/medicamento?cn=767491 ejemplo petición para 'Omeprazol STADA cn=767491'
     }
 
+    private fun receiveFullPharmaCode() {
+        parentFragmentManager.setFragmentResultListener(
+            requestKey,
+            this
+        ) { requestKey: String, bundle: Bundle ->
+            val fullCodeValue: String? = bundle.getString(code)
+            readCodeTv.setText(fullCodeValue).toString()
+            if (fullCodeValue != null) {
+                pharmaNatCode = extractPharmaNationalCode(fullCodeValue)
+            } else {
+                Toast.makeText(Context.context, "No se ha podido leer el código", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+    private fun extractPharmaNationalCode(fullCodeValue: String): String {
+        val chArr: CharArray? = fullCodeValue?.toCharArray()
+        val natCode: CharArray? = CharArray(6)
+        var n: Int = 0
+        for (i in 10..15) {
+            chArr?.get(i)?.let {
+                natCode?.set(n, it)
+                n++
+            }
+        }
+        // Test.
+
+        var str: String = ""
+        if (natCode != null) {
+            for (i in natCode.indices) {
+                print("*********   ${natCode[i]}")
+                str = str.plus(natCode[i].toString())
+            }
+            print(str)
+        }
+        return str
+    }
 }
